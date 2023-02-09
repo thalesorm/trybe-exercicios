721b2ded34b84e12a1c3803b859a064991aac7d4
Thales Santos <thalis.mraz@gmail.com>
2023-02-09 17:04:20 -0300

    feat: exercicios do dia 2 intro javascript

---
+++ 1-fundamentos/secao-03-introducao-a-Javascript/javascript-primeiros-passos/exercicio1.js
@@ -0,0 +1,15 @@
+// Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+let a = 5;
+let b = 10;
+
+console.log('Soma: ' + (a + b));
+console.log('Subtração: ' + (a - b));
+console.log('Multiplicação:' + (a * b));
+console.log('Divisão: ' + (a / b));
+console.log('Mudulo: ' + (a % b));
